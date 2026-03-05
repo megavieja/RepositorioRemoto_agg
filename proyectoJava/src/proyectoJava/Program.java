2be1b23de3dc6662d5f6da7d382a8da090082b79
alumno <alumno@alumno-Lenovo-V14-G5-IRL>
2026-03-05 11:27:53 +0100

    Cambio2

--- proyectoJava/src/proyectoJava/Program.java
+++ proyectoJava/src/proyectoJava/Program.java
@@ -5,6 +5,9 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hola mundo");
+		System.out.print("Adios mundo");
+		System.out.print("WAZAAAAAAA");
+		
 	}
 
 }
